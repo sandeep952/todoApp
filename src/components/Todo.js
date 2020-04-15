5b3c78d227e4517196cd945616dce539334b3b58
Sandeep Sharma <sandy335582@gmail.com>
2020-04-15 20:39:58 +0530

    added styling to todo

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -2,11 +2,17 @@ import React from "react";
 
 const Todo = (props) => {
   return (
-    <div className="card my-3">
-      <div className="card-body">
-          <input type="checkbox"  />
-          <span className="px-2">{props.title}</span>
-      </div>
+    <div className="card my-3 p-3">
+        <div className="custom-control custom-checkbox">
+          <input
+            type="checkbox"
+            className="custom-control-input"
+            id={props.id}
+          />
+          <label className="custom-control-label" htmlFor={props.id}>
+          {props.title}
+          </label>
+        </div>
     </div>
   );
 };
--- src/components/Todos.js
+++ src/components/Todos.js
@@ -4,10 +4,10 @@ import Todo from './Todo';
 const Todos = (props)=>{
     return (
         <div>
-        <Todo title ="Learn algorithms"/>
-        <Todo title ="Learn algorithms"/>
-        <Todo title ="Learn algorithms"/>
-        <Todo title ="Learn algorithms"/>
+        <Todo title ="Learn algorithms" id={1}/>
+        <Todo title ="Learn algorithms" id={2}/>
+        <Todo title ="Learn algorithms" id={3}/>
+        <Todo title ="Learn algorithms" id={4}/>
     
         </div>
     )
